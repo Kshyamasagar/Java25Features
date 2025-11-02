8a5c0e573c63b043a6d86ffb317a42be1483ad5c
Kshyamasagar <kshyamasagar.mishra09@gmail.com>
2025-11-02 10:57:48 +0530

    instance main method

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
-    public static void main(String[] arg){
-        System.out.println("Hello World");
+//    public static void main(String[] arg){
+//        System.out.println("Hello World");
+//    }
+    //Instance Main Method
+    void main(){
+        System.out.println("Main method without access modifier");
     }
 }
